bd1b8d260d2f1f96626e0a0d5c5feb248230e29b
Eladio Salazar Suarez <esalazars@unal.edu.co>
2023-12-01 23:56:37 -0500

    Commit cambios raros

--- moncare/base/urls.py
+++ moncare/base/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from base.views import home, logout, perfil
+from base.views import home, logout, perfil, get_empleados
 
 urlpatterns = [
     path('Moncare/', home, name='pagina_principal'),
     path('Moncare/logout/', logout, name='logout'),
     path('Moncare/perfil/', perfil, name='perfil'),
-
+    path('Moncare/gestor-dispositivos/', get_empleados, name='gestor_dispositivos')
 ]
