dc1e344f40616f6d7e5b581df52054031a6e246d
Eladio Salazar Suarez <esalazars@unal.edu.co>
2023-12-03 12:31:57 -0500

    Revirtiendo cambios Pequeños

--- moncare/API/admin.py
+++ moncare/API/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from base.models import Dispositivo_Medico
+from base.models import Dispositivo_Medico, Medicion
 # Register your models here.
 admin.site.register(Dispositivo_Medico)
+admin.site.register(Medicion)
--- moncare/API/urls.py
+++ moncare/API/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from API import views
+from API.views import MedicionViewSet, DispositivoMedicoViewSet
 
 router = routers.DefaultRouter()
-router.register(r'dispositivo_medico', views.DispositivoMedicoViewSet)
-router.register(r'medicion', views.MedicionViewSet)
+router.register(r'dispositivo_medico', DispositivoMedicoViewSet)
+router.register(r'medicion', MedicionViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
